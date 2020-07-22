a3ad02a103897cbb26445dcca15affa3b062c2f2
Drew Merrill <drewcmerrill@gmail.com>
2020-07-22 13:12:43 -0700

    Made a random walker using vectors. Added Levy Flight.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+var pos;
+var prev;
+var vel;
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(400, 400);
+	background(51);
+	pos = createVector(200,200);
+	prev = pos.copy();
 }
 
 function draw() {
+	//background(51);
+	stroke(0,0,255);
+	strokeWeight(2);
+	//point(pos.x, pos.y);
+	line(pos.x, pos.y, prev.x, prev.y);
+	prev.set(pos);
+	vel = p5.Vector.random2D();
+
+	var r = random(100);
+	if(r < 1) //there's a 1% chance it will make a jump. The jump will be between 1 and 100 pixels.
+	{
+		vel.mult(random(1,100));
+	}
+	pos.add(vel);
 
 }
